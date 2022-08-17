4d2712a58c6018be8594e33507e18d1b845b41fc
Khari2002 <91864459+Khari2002@users.noreply.github.com>
2022-08-17 11:33:50 +0530

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,8 @@
+
+adj=input("Adjective: ")
+sub1=input("subject: ")
+sub2=input("subject: ")
+person_name=input("Person name: ")
+madlib=f"I love computer programming because it makes me so {adj}! It makes me feel so {sub1} and it makes me feel so {sub2}. So please stay hygenic and active like {person_name}!"
+
+print(madlib)
